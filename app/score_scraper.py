58d1021417ffeccc32eca19303bb34b40efa61ca
Josh Mosier <jmosier@kadsci.com>
2019-02-24 11:57:46 -0500

    Updated score_scraper
    No longer uses selenium and instead pulls data from sports-reference.com

--- app/score_scraper.py
+++ app/score_scraper.py
@@ -1,30 +1,21 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import re
-import os
+import requests
 
-#launch url
-url = "http://www.espn.com/mens-college-basketball/scoreboard"
-
-# create a new Firefox session
 def get_scoreticker():
-	driver = webdriver.Chrome()
-	driver = webdriver.Chrome('C:/Users/joshr/OneDrive/Documents/GitHub/MosierMadness/app/chromedriver.exe') 
-	driver.get(url)
+	sauce = requests.get("https://www.sports-reference.com/cbb/")
+	soup = BeautifulSoup(sauce.content,'html.parser')
+	games = soup.find("div",{"class": "game_summaries"})
+	winners = games.findAll("tr",{"class": "winner"})
+	losers = games.findAll("tr",{"class": "loser"})
+	list_winners = []
+	list_losers =  []
+	for game in winners:
+		list_winners.append([game.find("a").getText(),game.find("td",{"class": "right"}).getText()])
 
-	a=[];
-	a = driver.find_elements_by_class_name("competitors")
+	for game in losers:
+		list_losers.append([game.find("a").getText(),game.find("td",{"class": "right"}).getText()])
 
-	soup = BeautifulSoup(driver.page_source)
-	score_tables = soup.find("div", {"id": "events"})
-	teams = score_tables.findAll("span",{"class": "sb-team-abbrev"})
-	scores = score_tables.findAll("td",{"class": "total"})
-	teams_stripped = []
-	scores_stripped = []
-	for i,team in enumerate(teams):
-			teams_stripped.append(teams[i].getText())
-			scores_stripped.append(scores[i].getText())
+	print(list_winners)
+	return [list_winners,list_losers]
 
-	driver.close()
-	return [teams_stripped,scores_stripped]
+get_scoreticker()
