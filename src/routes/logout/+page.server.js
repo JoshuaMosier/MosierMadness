78a919b0703e699d46f5e2e5dd0182b166b4ac7c
Josh Mosier <josh.r.mosier@gmail.com>
2025-03-04 16:26:39 -0500

    reset db funcs

--- src/routes/logout/+page.server.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export function load() {
-  // This ensures that if someone tries to access this page directly without JavaScript,
-  // they will still be redirected to the home page
-  throw redirect(302, '/');
-} 
